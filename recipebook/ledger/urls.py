d77eca088d169683937e2fa2d71cb09834299b74
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2024-02-15 17:06:55 +0800

    updated urls.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import recipe1, recipe2, recipes_list
+
 urlpatterns = [
-path('', index, name='index'),
+path('recipes/list', recipes_list, name='recipes_list'),
+path('recipe/1', recipe1, name='recipes_list'),
+path('recipe/2', recipe2, name='recipes_list'),
+
 ]
-# This might be needed, depending on your Django version
+
 app_name = "ledger"
