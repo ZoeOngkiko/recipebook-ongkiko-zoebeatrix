6f7459ba33d6079fde479ba87366d6f54a7f73eb
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2024-02-15 22:19:52 +0800

    removed unnecessary lines

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# def index(request):
-#     return HttpResponse('Hello World! This came from the index view')
 
 def recipes_list(request):
     context = {
