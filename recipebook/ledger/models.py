1c716b01bcd83d1892e2c94927159464c9470c01
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2024-03-04 12:56:35 +0800

    set the string representations and absolute urls of the Ingredient and Recipe models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ingredient', args=[str(self.name)])   #returns the URL with the specified name, and can pass arguments to the function
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
